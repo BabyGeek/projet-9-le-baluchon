b08065bc23463a39359182cc2b2e798d0e3d2647
OGGERO Paul <paul.oggero@softeam.fr>
2022-07-12 20:24:51 +0200

    First Commit: Created application and files architecture

---
+++ LeBaluchon/LeBaluchonApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeBaluchonApp.swift
+//  LeBaluchon
+//
+//  Created by Paul Oggero on 12/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct LeBaluchonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            WeatherView()
+        }
+    }
+}
---
+++ LeBaluchon/Views/Weather/WeatherView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LeBaluchon
+//
+//  Created by Paul Oggero on 12/07/2022.
+//
+
+import SwiftUI
+
+struct WeatherView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct WeatherView_Previews: PreviewProvider {
+    static var previews: some View {
+        WeatherView()
+    }
+}
