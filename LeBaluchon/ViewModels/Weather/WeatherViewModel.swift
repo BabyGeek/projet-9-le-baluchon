498d3e07d860f648420e43d8f737fe33120c916e
OGGERO Paul <paul.oggero@softeam.fr>
2022-07-13 22:44:05 +0200

    feat(weather): added view model for weather and first call

---
+++ LeBaluchon/ViewModels/Weather/WeatherViewModel.swift
@@ -0,0 +1,48 @@
+//
+//  WeatherManager.swift
+//  LeBaluchon
+//
+//  Created by Paul Oggero on 12/07/2022.
+//
+
+import Foundation
+
+
+class WeatherViewModel: ObservableObject {
+    @Published var weathers: [Weather] = [Weather]()
+    private let url: String = "https://api.openweathermap.org/data/2.5/weather?lang=fr&units=metric"
+    private let apiKey: String = "1e3be892b4867f22812876984dd1d18f"
+
+    public func perform(lat latitude: Double, lon longitude: Double) {
+        var url = self.url
+        url.append("&lat=\(latitude)")
+        url.append("&lon=\(longitude)")
+        url.append("&appid=\(self.apiKey)")
+        
+        print(url)
+        
+        guard let url = URL(string: url) else {
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data, error == nil else {
+                return
+            }
+            
+            // Convert to JSON
+            do {
+                let weather = try JSONDecoder().decode(Weather.self, from: data)
+                
+                DispatchQueue.main.async {
+                    self.weathers.append(weather)
+                }
+            } catch {
+                print(error)
+            }
+        }
+        
+        task.resume()
+    }
+}
+
