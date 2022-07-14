15e2ca64159783a09208ba9a3cd1de0d6e02f7d6
OGGERO Paul <paul.oggero@softeam.fr>
2022-07-14 19:42:09 +0200

    feat(weather): WIP updated and refactored row view

--- LeBaluchon/Views/Weather/WeatherRowView.swift
+++ LeBaluchon/Views/Weather/WeatherRowView.swift
@@ -12,42 +12,49 @@ struct WeatherRowView: View {
     
     var body: some View {
         
+        HStack(alignment: .center, spacing: 8) {
+            weather.weather.first!.symbol
+                .font(.system(size: 44))
+            
+            Spacer()
+            
+            VStack(alignment: .center, spacing: 8) {
+                Text(weather.name)
+                    .font(.title3)
+                
+                MainContent(weather: weather.main)
+            }
+            
+            Spacer()
+            Divider()
+            
+            Text(weather.weather.first!.description)
+                .font(.caption)
+                .frame(maxWidth: 50)
+        }
+        .clipShape(Capsule())
+    }
+}
+
+struct MainContent: View {
+    let weather: WeatherMain
+    
+    var body: some View {
+        VStack(alignment: .center, spacing: 8) {
+            Text("\(weather.temp.formatted())°C")
             HStack(alignment: .center, spacing: 8) {
-                weather.weather.first!.symbol
-                    .font(.system(size: 44))
-                
-                Spacer()
-                
-                VStack(alignment: .center, spacing: 8) {
-                    Text(weather.name)
-                        .font(.title3)
-                                        
-                    
-                    VStack(alignment: .center, spacing: 8) {
-                        Text("\(weather.main.temp.formatted())°C")
-                        HStack(alignment: .center, spacing: 8) {
-                            HStack(alignment: .center, spacing: 8) {
-                                Image(systemName: "thermometer.sun.fill")
-                                Text("\(Int(weather.main.tempMax.rounded()))°C")
-                                    .font(.caption2)
-                            }
-                            
-                            HStack(alignment: .center, spacing: 8) {
-                                Image(systemName: "thermometer.snowflake")
-                                Text("\(Int(weather.main.tempMin.rounded()))°C")
-                                    .font(.caption2)
-                            }
-                        }
-                    }
-                    
+                HStack(alignment: .center, spacing: 8) {
+                    Image(systemName: "thermometer.sun.fill")
+                    Text("\(Int(weather.tempMax.rounded()))°C")
+                        .font(.caption2)
                 }
                 
-                Spacer()
-                Divider()
-                
-                Text(weather.weather.first!.description)
-                    .font(.caption)
-                    .frame(maxWidth: 50)
+                HStack(alignment: .center, spacing: 8) {
+                    Image(systemName: "thermometer.snowflake")
+                    Text("\(Int(weather.tempMin.rounded()))°C")
+                        .font(.caption2)
+                }
             }
+        }
     }
 }
