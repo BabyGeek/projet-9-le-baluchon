30ad9435cf3d92d472025d1104ecaa6a0ef07e1f
OGGERO Paul <paul.oggero@softeam.fr>
2022-07-14 12:33:14 +0200

    feat(weather): refactored row and corrected warning issues

---
+++ LeBaluchon/Views/Weather/WeatherRowView.swift
@@ -0,0 +1,15 @@
+//
+//  WeatherRowView.swift
+//  LeBaluchon
+//
+//  Created by Paul Oggero on 13/07/2022.
+//
+
+import SwiftUI
+
+
+struct WeatherRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        WeatherRowView()
+    }
+}
--- LeBaluchon/Views/Weather/WeatherView.swift
+++ LeBaluchon/Views/Weather/WeatherView.swift
@@ -11,22 +11,28 @@ struct WeatherView: View {
     @StateObject var viewModel = WeatherViewModel()
     
     var body: some View {
-        NavigationView {
-            List(viewModel.weathers, id: \.self) { weather in
-                HStack {
-                    Text(weather.name)
-                    Text("\(weather.main.temp)")
-                }
-                .padding(3)
-            }
+        List(viewModel.weathers, id: \.self) { weather in
+            WeatherRowView(weather: weather)
         }
-        .navigationTitle("Météo")
         .onAppear {
             viewModel.perform(lat: 35, lon: 13)
+            viewModel.perform(lat: 13, lon: 35)
+            viewModel.perform(lat: 35, lon: 139)
         }
     }
 }
 
+struct WeatherRowView: View {
+    let weather: Weather
+    
+    var body: some View {
+        HStack {
+            Text(weather.name)
+            Text("\(weather.main.temp.formatted())")
+        }
+        .padding(3)
+    }
+}
 
 struct WeatherView_Previews: PreviewProvider {
     static var previews: some View {
