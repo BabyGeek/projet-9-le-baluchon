d7e00eadfe660a03a3ed60c08f9abe4d80780eb4
OGGERO Paul <paul.oggero@softeam.fr>
2022-07-17 23:08:22 +0200

    feat(currency): Modified View and ViewModel to connect API, display and update exchange currencies or amount

--- LeBaluchon/ViewModels/Currency/CurrencyViewModel.swift
+++ LeBaluchon/ViewModels/Currency/CurrencyViewModel.swift
@@ -14,7 +14,7 @@ class CurrencyViewModel: ObservableObject {
     private let url: String = ApiConstants.currencyAPIURL
     private let apiKey: String = ApiConstants.currencyAPIKEY
     
-    var result: ExchangeRateResult?
+    @Published var result: ExchangeRateResult?
     
     
     public func perform(from: String, to: String) {
@@ -22,20 +22,36 @@ class CurrencyViewModel: ObservableObject {
     }
     
     public func performFor(from: String, to: String, amount: Double) {
-        print("done")
-        getResult()
-    }
-    
-    private func getResult() {
-        self.result = ExchangeRateResult(value: 0)
-    }
-    
-    public func getResultValue() -> Double {
-        if let result = result {
-            return result.value
+        var url = self.url
+        
+        url.append("?apikey=\(apiKey)")
+        url.append("&to=\(to)")
+        url.append("&from=\(from)")
+        url.append("&amount=\(amount)")
+        
+        guard let url = URL(string: url) else {
+            return
         }
         
-        return 0
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data, error == nil else {
+                print(error!.localizedDescription)
+                return
+            }
+            
+            // Convert to JSON
+            do {
+                let result = try JSONDecoder().decode(ExchangeRateResult.self, from: data)
+                
+                DispatchQueue.main.async {
+                    self.result = result
+                }
+            } catch {
+                print(error)
+            }
+        }
+        
+        task.resume()
     }
 }
 
--- LeBaluchon/Views/Currency/CurrencyView.swift
+++ LeBaluchon/Views/Currency/CurrencyView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct CurrencyView: View {
-    private var viewModel = CurrencyViewModel()
+    @StateObject private var viewModel = CurrencyViewModel()
     @State var from: String = "EUR"
     @State var to: String = "USD"
     @State var amount: Double = 1
@@ -41,21 +41,34 @@ extension CurrencyView {
             Section("Convert a currency") {
                 TextField("Amount", value: $amount, format: .currency(code: from))
                     .keyboardType(.decimalPad)
+                    .onChange(of: amount) { newValue in
+                        viewModel.performFor(from: from, to: to, amount: newValue)
+                    }
                 
                 Picker("From", selection: $from) {
                     Text("USD").tag("USD")
                     Text("EUR").tag("EUR")
                     Text("VND").tag("VND")
                 }
+                .onChange(of: from) { newValue in
+                    viewModel.performFor(from: newValue, to: to, amount: amount)
+                }
+                
+                
                 Picker("To", selection: $to) {
                     Text("USD").tag("USD")
                     Text("EUR").tag("EUR")
                     Text("VND").tag("VND")
                 }
+                .onChange(of: to) { newValue in
+                    viewModel.performFor(from: from, to: newValue, amount: amount)
+                }
             }
             
             Section("Result") {
-                Text("\(amount.formatted(.currency(code: from))) = \(viewModel.getResultValue().formatted(.currency(code: to))) ")
+                if let result = viewModel.result {
+                    Text("\(amount.formatted(.currency(code: from))) = \(result.result.formatted(.currency(code: to))) ")
+                }
             }
         }
     }
