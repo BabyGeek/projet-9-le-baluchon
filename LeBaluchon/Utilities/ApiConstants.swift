3dc5b7a52b8394f86b5862ab3f675d8fe159c1ce
OGGERO Paul <paul.oggero@softeam.fr>
2022-07-17 23:06:56 +0200

    feat(currency): WIP added API settings

--- LeBaluchon/Utilities/ApiConstants.swift
+++ LeBaluchon/Utilities/ApiConstants.swift
@@ -11,6 +11,6 @@ struct ApiConstants {
     static let weatherAPIURL = "https://api.openweathermap.org/data/2.5/weather"
     static let weatherAPIKEY = "1e3be892b4867f22812876984dd1d18f"
     
-    static let currencyAPIURL = ""
-    static let currencyAPIKEY = ""
+    static let currencyAPIURL = "https://api.apilayer.com/fixer/convert"
+    static let currencyAPIKEY = "4ItdMbpqLI7tYaPfDK2enMVkQ1j7tqNp"
 }
