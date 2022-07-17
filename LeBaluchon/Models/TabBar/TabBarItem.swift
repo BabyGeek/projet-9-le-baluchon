0f42c157c71f3119a078a5662971603d9de6d823
OGGERO Paul <paul.oggero@softeam.fr>
2022-07-17 14:40:30 +0200

    feat(tab bar): make TabBarItem an Enum

--- LeBaluchon/Models/TabBar/TabBarItem.swift
+++ LeBaluchon/Models/TabBar/TabBarItem.swift
@@ -6,3 +6,38 @@
 //
 
 import Foundation
+import SwiftUI
+
+//struct TabBarItem: Hashable {
+//    let iconName: String
+//    let title: String
+//    let color: Color
+//}
+
+enum TabBarItem: Hashable {
+    case weather, exchange, translate
+    
+    var iconName: String {
+        switch self {
+        case .weather: return "house"
+        case .exchange: return "heart"
+        case .translate: return "person"
+        }
+    }
+    
+    var title: String {
+        switch self {
+        case .weather: return "Weather"
+        case .exchange: return "Exchange"
+        case .translate: return "Translate"
+        }
+    }
+    
+    var color: Color {
+        switch self {
+        case .weather: return .red
+        case .exchange: return .blue
+        case .translate: return .green
+        }
+    }
+}
