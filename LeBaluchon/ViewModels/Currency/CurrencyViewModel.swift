d49765f77eb5c98408aa6c54eb41e87e4612b60c
OGGERO Paul <paul.oggero@softeam.fr>
2022-07-17 17:10:55 +0200

    feat(currency): WIP basic view and mock data

--- LeBaluchon/ViewModels/Currency/CurrencyViewModel.swift
+++ LeBaluchon/ViewModels/Currency/CurrencyViewModel.swift
@@ -14,10 +14,28 @@ class CurrencyViewModel: ObservableObject {
     private let url: String = ApiConstants.currencyAPIURL
     private let apiKey: String = ApiConstants.currencyAPIKEY
     
-    var result: CurrencyResult?
+    var result: ExchangeRateResult?
     
     
-    public func perform(lat latitude: Double, lon longitude: Double, setTarget: Bool = false) {
+    public func perform(from: String, to: String) {
+        self.performFor(from: from, to: to, amount: 1)
+    }
+    
+    public func performFor(from: String, to: String, amount: Double) {
+        print("done")
+        getResult()
+    }
+    
+    private func getResult() {
+        self.result = ExchangeRateResult(value: 0)
+    }
+    
+    public func getResultValue() -> Double {
+        if let result = result {
+            return result.value
+        }
+        
+        return 0
     }
 }
 
--- LeBaluchon/Views/Currency/CurrencyView.swift
+++ LeBaluchon/Views/Currency/CurrencyView.swift
@@ -8,13 +8,55 @@
 import SwiftUI
 
 struct CurrencyView: View {
+    private var viewModel = CurrencyViewModel()
+    @State var from: String = "EUR"
+    @State var to: String = "USD"
+    @State var amount: Double = 1
+    
+    @State private var isLoading = false
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack(spacing: 8) {
+                form
+            }
+            .navigationTitle("Exchange")
+        }
+        .onAppear {
+            viewModel.perform(from: "EUR", to: "USD")
+        }
     }
 }
 
 struct CurrencyView_Previews: PreviewProvider {
     static var previews: some View {
         CurrencyView()
+    }
+}
+
+extension CurrencyView {
+    private var form: some View {
+        Form {
+            Section("Convert a currency") {
+                TextField("Amount", value: $amount, format: .currency(code: from))
+                    .keyboardType(.decimalPad)
+                
+                Picker("From", selection: $from) {
+                    Text("USD").tag("USD")
+                    Text("EUR").tag("EUR")
+                    Text("VND").tag("VND")
+                }
+                Picker("To", selection: $to) {
+                    Text("USD").tag("USD")
+                    Text("EUR").tag("EUR")
+                    Text("VND").tag("VND")
+                }
+            }
+            
+            Section("Result") {
+                Text("\(amount.formatted(.currency(code: from))) = \(viewModel.getResultValue().formatted(.currency(code: to))) ")
+            }
+        }
     }
 }
