17c0e861d798176c5a39d91bf427c84a93964186
OGGERO Paul <paul.oggero@softeam.fr>
2022-07-17 15:35:35 +0200

    feat(apiUtilities): added APIs constants file

---
+++ LeBaluchon/Utilities/ApiConstants.swift
@@ -0,0 +1,13 @@
+//
+//  ApiConstants.swift
+//  LeBaluchon
+//
+//  Created by Paul Oggero on 17/07/2022.
+//
+
+import Foundation
+
+struct ApiConstants {
+    static let weatherAPIURL = "https://api.openweathermap.org/data/2.5/weather"
+    static let weatherAPIKEY = "1e3be892b4867f22812876984dd1d18f"
+}
