aafacb14c6bcf5145677565f3ad74186cae813fa
OGGERO Paul <paul.oggero@softeam.fr>
2022-07-19 21:31:54 +0200

    hotfix: Add NetworkManager and loadData

---
+++ LeBaluchon/Utilities/NetworkManager.swift
@@ -0,0 +1,33 @@
+//
+//  NetworkManager.swift
+//  LeBaluchon
+//
+//  Created by Paul Oggero on 19/07/2022.
+//
+
+import Foundation
+
+
+class NetworkManager {
+    func loadData<T:Decodable>(urlRequest url: URL, onSuccess success: @escaping (T) -> (), onFailure failure: @escaping (Error) -> ()) {
+        let task = URLSession.shared.dataTask(with: url)
+        {
+            data, response, error in
+            
+            guard let data = data, error == nil else {
+                failure(error!)
+                return
+            }
+            
+            do {
+                let object = try JSONDecoder().decode(T.self, from: data)
+                success(object)
+            } catch {
+                failure(error)
+            }
+            
+        }
+        
+        task.resume()
+    }
+}
