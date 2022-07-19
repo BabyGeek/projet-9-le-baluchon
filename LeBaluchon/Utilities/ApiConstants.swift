abefda45c24186b674030e08572538576bb03212
OGGERO Paul <paul.oggero@softeam.fr>
2022-07-19 16:36:52 +0200

    feat(currency): Updated API to have more requests possibilities

--- LeBaluchon/Utilities/ApiConstants.swift
+++ LeBaluchon/Utilities/ApiConstants.swift
@@ -11,6 +11,6 @@ struct ApiConstants {
     static let weatherAPIURL = "https://api.openweathermap.org/data/2.5/weather"
     static let weatherAPIKEY = "1e3be892b4867f22812876984dd1d18f"
     
-    static let currencyAPIURL = "https://api.apilayer.com/fixer/convert"
-    static let currencyAPIKEY = "4ItdMbpqLI7tYaPfDK2enMVkQ1j7tqNp"
+    static let currencyAPIURL = "https://v6.exchangerate-api.com/v6/{apiKey}/{resource}"
+    static let currencyAPIKEY = "ad8787738ffa75a78f552c6f"
 }
