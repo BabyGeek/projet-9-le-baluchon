67c161c218b4594f15fc5d018692ea1c4e91c4b2
OGGERO Paul <paul.oggero@softeam.fr>
2022-07-21 18:30:56 +0200

    feat(errors-UnitTests): Add Network errors unit tests

--- LeBaluchonTests/NetworkUnitTests.swift
+++ LeBaluchonTests/NetworkUnitTests.swift
@@ -5,31 +5,53 @@
 //  Created by Paul Oggero on 21/07/2022.
 //
 
+@testable import LeBaluchon
 import XCTest
 
 class NetworkUnitTests: XCTestCase {
+    var manager: NetworkManager!
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    @MainActor override func setUp() {
+        super.setUp()
+        manager = NetworkManager()
     }
+    
+    func testGivenValidURLThenDecodeWithWrongDecoableShouldReturnFailureError() {
+        let url = Bundle.main.url(forResource: "symbols", withExtension: "json")!
+        let expectation = XCTestExpectation()
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+        manager.loadData(urlRequest: url) { (_: Weather) in
+        } onFailure: { error in
+            XCTAssertEqual(error, NetworkError.failure)
+            expectation.fulfill()
         }
+        
+        wait(for: [expectation], timeout: 3)
     }
+    
+    func testGivenWrongResponseURLThenFetchingShouldReturnLoadDataError() {
+        let expectation = XCTestExpectation()
 
+        manager.loadData(urlRequest: URL(string: "test.fr")!) { (_: Weather) in
+        } onFailure: { error in
+            XCTAssertEqual(error, NetworkError.loadDataError)
+            expectation.fulfill()
+        }
+        
+        wait(for: [expectation], timeout: 3)
+    }
+    
+    func testGivenErrorFailureThenCompareGetterwithStringsShouldBeEquals() {
+        let error = NetworkError.failure
+        
+        XCTAssertEqual(error.failureReason, NetworkError.failure.failureReason)
+        XCTAssertEqual(error.errorDescription, NetworkError.failure.errorDescription)
+        XCTAssertEqual(error.recoverySuggestion, NetworkError.failure.recoverySuggestion)
+    }
+    
+    func testGivenErrorThenCreateAppErrorShouldReturnIdentifiableUUIDObject() {
+        let appError = AppError(error: NetworkError.unknown)
+        
+        XCTAssertNotNil(appError.id)
+    }
 }
