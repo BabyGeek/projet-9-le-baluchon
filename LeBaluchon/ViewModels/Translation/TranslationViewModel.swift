680f4732d067e4ab562ee07a61b058b58acb84a4
OGGERO Paul <paul.oggero@softeam.fr>
2022-07-22 02:04:35 +0200

    feat(translation): WIP view and viewmodel for translations

--- LeBaluchon/ViewModels/Translation/TranslationViewModel.swift
+++ LeBaluchon/ViewModels/Translation/TranslationViewModel.swift
@@ -9,8 +9,12 @@ import Foundation
 
 class TranslationViewModel: NetworkManager, ObservableObject {
     @Published var error: AppError? = nil
-    @Published var result = ""
+    @Published var results: [Translation]? = nil
+    @Published var autoloadSource: Bool = true
     @Published var langs: [TranslationLanguage] = [TranslationLanguage]()
+    @Published var source: String = "en"
+    @Published var target: String = "fr"
+    
     
     private let url: String = ApiConstants.translationAPIURL
     private let apiKey: String = ApiConstants.translationAPIKEY
@@ -23,17 +27,68 @@ class TranslationViewModel: NetworkManager, ObservableObject {
         self.getLangs()
     }
     
-    #if DEBUG
-    public func perform() {
+#if DEBUG
+    public func performForText(_ text: String) {
+        let params = [
+            URLQueryItem(name: "target", value: self.target),
+            URLQueryItem(name: "source", value: self.source),
+            URLQueryItem(name: "q", value: text)
+        ]
         
+        if let url = self.getURL(resource: nil, params: params) {
+            self.loadData(urlRequest: url) { (translationData: TranslationData) in
+                self.results = translationData.data.translations
+            } onFailure: { error in
+                self.error = AppError(error: error)
+            }
+
+        }
     }
-    #endif
+#endif
     
     private func getLangs() {
+        if self.languagesLoaded {
+            return
+        }
         
+        if let url = self.getURL(resource: "languages", params: [URLQueryItem(name: "target", value: self.source)]) {
+            self.loadData(urlRequest: url) { (languageDictionnary: TranslationLanguageData) in
+                self.langs = languageDictionnary.data.languages.sorted(by: { $0.language < $1.language })
+            } onFailure: { error in
+                self.error = AppError(error: error)
+            }
+            
+        }
     }
     
-    private func getURLWithParams(_ params: [Any]?) {
+    /// Build the API query related to additional parameters given
+    /// - Parameters:
+    ///   - resource: Type of resource to reach in the api
+    ///   - params: Optional array of params
+    /// - Returns: Optional URL
+    private func getURL(resource: String? = nil, params: [URLQueryItem]? = nil) -> URL? {
+        var url = self.url
         
+        if let resource = resource {
+            url.append("/\(resource)")
+        }
+        
+        var urlComponent = URLComponents(string: url)
+        
+        
+        let baseParams = [
+            URLQueryItem(name: "key", value: self.apiKey),
+        ]
+        if let params = params {
+            urlComponent?.queryItems = params + baseParams
+        } else {
+            urlComponent?.queryItems = baseParams
+        }
+        
+        if let urlComponent = urlComponent {
+            return urlComponent.url
+        }
+        
+        return nil
     }
 }
--- LeBaluchon/Views/Translation/TranslationView.swift
+++ LeBaluchon/Views/Translation/TranslationView.swift
@@ -5,11 +5,48 @@
 //  Created by Paul Oggero on 21/07/2022.
 //
 
+#if DEBUG
 import SwiftUI
 
 struct TranslationView: View {
+    @StateObject private var viewModel = TranslationViewModel()
+    @State var source: String = "en"
+    @State var target: String = "fr"
+    @State var text: String = "Hello my name is Paul, I am happy to see you here"
+    
+    @State var selectTarget = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            form
+                .navigationTitle("Translation")
+                .toolbar {
+                    ToolbarItem {
+                        Button(action: {
+                            self.viewModel.autoloadSource.toggle()
+                        }, label: {
+                            Image(systemName: self.viewModel.autoloadSource ? "star.bubble.fill" : "star.bubble")
+                        })
+                    }
+                }
+        }
+        .sheet(isPresented: $selectTarget) {
+            SelectTargetSheet(target: target, viewModel: self.viewModel, text: text)
+        }
+        .onAppear {
+            //
+        }
+        .alert(item: $viewModel.error) { error in
+            guard let descrition = error.error.errorDescription, let message = error.error.failureReason else {
+                return Alert(
+                    title: Text(NetworkError.unknown.errorDescription!),
+                    message: Text(NetworkError.unknown.failureReason!))
+            }
+            
+            return Alert(
+                title: Text(descrition),
+                message: Text(message))
+        }
     }
 }
 
@@ -18,3 +55,102 @@ struct TranslationView_Previews: PreviewProvider {
         TranslationView()
     }
 }
+
+extension TranslationView {
+    private var form: some View {
+        Form {
+            if !self.viewModel.autoloadSource {
+                Section("Source") {
+                    
+                    Picker("Source language", selection: $source) {
+                        
+                        ForEach($viewModel.langs.indices, id: \.self) { index in
+                            let lang = viewModel.langs[index]
+                            langListView(lang: lang)
+                        }
+                    }
+                }
+            }
+            
+            
+            Section("Translate") {
+                TextEditor(text: $text)
+                    .frame(minHeight: 60, alignment: .leading)
+                    .foregroundColor(.black)
+                    .multilineTextAlignment(.leading)
+                    
+                
+                Button("Translate") {
+                    self.selectTarget = true
+                }
+            }
+            
+            Section("Translation") {
+                if let results = viewModel.results {
+                    Text(results.first!.translatedText)
+                }else if viewModel.isLoading {
+                    Text("Loading...")
+                }else {
+                    Text("Nothing to translate yet")
+                }
+            }
+        }
+    }
+}
+
+struct SelectTargetSheet: View {
+    @Environment(\.dismiss) var dismiss
+    @State var target: String
+    @StateObject var viewModel: TranslationViewModel
+    var text: String
+    
+    var body: some View {
+        
+        NavigationView {
+            Form {
+                Picker("Select a target language", selection: $target) {
+                    ForEach($viewModel.langs.indices, id: \.self) { index in
+                        let lang = viewModel.langs[index]
+                        langListView(lang: lang)
+                            .id(lang.language)
+                    }
+                }
+                .pickerStyle(.wheel)
+                
+                Button("Translate") {
+                    self.viewModel.target = target
+                    self.viewModel.performForText(text)
+                    self.dismiss()
+                }
+            }
+            .navigationTitle("Target language")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem {
+                    Button(action: {
+                        self.dismiss()
+                    }, label: {
+                        Image(systemName: "xmark")
+                    })
+                }
+            }
+        }
+        
+    }
+    
+    func targetSelected(lang: TranslationLanguage) {
+        
+    }
+}
+
+/// Lang view for select list
+struct langListView: View {
+    let lang: TranslationLanguage
+    
+    var body: some View {
+        Text("\(lang.name) - \(lang.language.uppercased())")
+            .tag(lang.language)
+    }
+}
+
+#endif
