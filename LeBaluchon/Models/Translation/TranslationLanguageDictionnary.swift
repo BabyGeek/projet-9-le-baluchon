fffc4579ceee148ec65948dd015fc579007f50a0
OGGERO Paul <paul.oggero@softeam.fr>
2022-07-22 02:02:06 +0200

    feat(translation): WIP models for translation language datas

--- LeBaluchon/Models/Translation/TranslationLanguageDictionnary.swift
+++ LeBaluchon/Models/Translation/TranslationLanguageDictionnary.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+
+struct TranslationLanguageData: Codable, Hashable {
+    let data: TranslationLanguageDictionnary
+}
+
+struct TranslationLanguageDictionnary: Codable, Hashable {
+    let languages: [TranslationLanguage]
+}
+
+struct TranslationLanguage: Codable, Identifiable, Hashable {
+    let id: UUID
+    let language: String
+    let name: String
+    
+    
+    private enum CodingKeys: String, CodingKey {
+      case id, language, name
+    }
+      
+    init(from decoder: Decoder) throws {
+      let container = try decoder.container(keyedBy: CodingKeys.self)
+        id = try container.decodeIfPresent(UUID.self, forKey: .id) ?? UUID()
+        language = try container.decode(String.self, forKey: .language)
+        name = try container.decode(String.self, forKey: .name)
+    }
+}
