ae9f670c7f67c8e52eb268b88cef92b68deeb3e4
OGGERO Paul <paul.oggero@softeam.fr>
2022-07-24 00:00:40 +0200

    hotfix(View): Added view extensions for keyboard dismiss and landscape fullscreen support

--- LeBaluchon/Utilities/ViewExtensions.swift
+++ LeBaluchon/Utilities/ViewExtensions.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+import SwiftUI
+
+
+
+extension View {
+    /// Add extention to dismiss keyboard display on view click
+    func endTextEditing(including: GestureMask = .all) -> some View {
+        return self
+            .highPriorityGesture(TapGesture().onEnded({ _ in
+                UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder),
+                                                    to: nil, from: nil, for: nil)
+            }), including: including)
+    }
+}
+
+
+
+extension View {
+    /// Change the default behavior for landscape style view on iphone
+    @ViewBuilder func phoneOnlyNavigationView() -> some View {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            self.navigationViewStyle(.stack)
+        } else {
+            self
+        }
+    }
+}
