d6b190659bbe7f8f790586f8ee5e373628a39f05
OGGERO Paul <paul.oggero@softeam.fr>
2022-07-24 00:05:59 +0200

    hotfix(tests): Added all network errors test to have abetter coverage

--- LeBaluchonTests/NetworkUnitTests.swift
+++ LeBaluchonTests/NetworkUnitTests.swift
@@ -41,12 +41,36 @@ class NetworkUnitTests: XCTestCase {
         wait(for: [expectation], timeout: 3)
     }
     
-    func testGivenErrorFailureThenCompareGetterwithStringsShouldBeEquals() {
+    func testGivenErrorFailureThenCompareGetterWithStringsShouldBeEquals() {
         let error = NetworkError.failure
         
         XCTAssertEqual(error.failureReason, NetworkError.failure.failureReason)
         XCTAssertEqual(error.errorDescription, NetworkError.failure.errorDescription)
         XCTAssertEqual(error.recoverySuggestion, NetworkError.failure.recoverySuggestion)
+    }
+    
+    func testGivenErrorUnknownThenCompareGetterWithStringsShouldBeEquals() {
+        let error = NetworkError.unknown
+        
+        XCTAssertEqual(error.failureReason, NetworkError.unknown.failureReason)
+        XCTAssertEqual(error.errorDescription, NetworkError.unknown.errorDescription)
+        XCTAssertEqual(error.recoverySuggestion, NetworkError.unknown.recoverySuggestion)
+    }
+    
+    func testGivenErrorWrongURLThenCompareGetterWithStringsShouldBeEquals() {
+        let error = NetworkError.wrongURLError
+        
+        XCTAssertEqual(error.failureReason, NetworkError.wrongURLError.failureReason)
+        XCTAssertEqual(error.errorDescription, NetworkError.wrongURLError.errorDescription)
+        XCTAssertEqual(error.recoverySuggestion, NetworkError.wrongURLError.recoverySuggestion)
+    }
+    
+    func testGivenErrorWrongDataThenCompareGetterWithStringsShouldBeEquals() {
+        let error = NetworkError.loadDataError
+        
+        XCTAssertEqual(error.failureReason, NetworkError.loadDataError.failureReason)
+        XCTAssertEqual(error.errorDescription, NetworkError.loadDataError.errorDescription)
+        XCTAssertEqual(error.recoverySuggestion, NetworkError.loadDataError.recoverySuggestion)
     }
     
     func testGivenErrorThenCreateAppErrorShouldReturnIdentifiableObject() {
