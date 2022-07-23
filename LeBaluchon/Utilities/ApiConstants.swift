1277e65a3a871704dd9768ee691f7379bbd5dce9
OGGERO Paul <paul.oggero@softeam.fr>
2022-07-23 13:14:36 +0200

    updated api constants

--- LeBaluchon/Utilities/ApiConstants.swift
+++ LeBaluchon/Utilities/ApiConstants.swift
@@ -10,22 +10,11 @@ import Foundation
 /// API constants
 struct ApiConstants {
     static let weatherAPIURL = "https://api.openweathermap.org/data/2.5/weather"
-    static let weatherAPIKEY = "1e3be892b4867f22812876984dd1d18f"
+    static let weatherAPIKEY = "68afd04a325c72d8726f2cc2672a78c6"
     
     static let currencyAPIURL = "https://v6.exchangerate-api.com/v6/{apiKey}/{resource}"
     static let currencyAPIKEY = "ad8787738ffa75a78f552c6f"
     
     static let translationAPIURL = "https://translation.googleapis.com/language/translate/v2"
     static let translationAPIKEY = "AIzaSyBkcs1EDaphfKpPH2Zv5qVlsAzowjOMorg"
-    static let translationENBaseText = """
-        Do greatest at in learning steepest. Breakfast extremity suffering one who all otherwise suspected. He at no nothing forbade up moments. Wholly uneasy at missed be of pretty whence. John way sir high than law who week. Surrounded prosperous introduced it if is up dispatched. Improved so strictly produced answered elegance is.
-
-        Speedily say has suitable disposal add boy. On forth doubt miles of child. Exercise joy man children rejoiced. Yet uncommonly his ten who diminution astonished. Demesne new manners savings staying had. Under folly balls death own point now men. Match way these she avoid see death. She whose drift their fat off.
-
-        Or neglected agreeable of discovery concluded oh it sportsman. Week to time in john. Son elegance use weddings separate. Ask too matter formed county wicket oppose talent. He immediate sometimes or to dependent in. Everything few frequently discretion surrounded did simplicity decisively. Less he year do with no sure loud.
-
-        Inquietude simplicity terminated she compliment remarkably few her nay. The weeks are ham asked jokes. Neglected perceived shy nay concluded. Not mile draw plan snug next all. Houses latter an valley be indeed wished merely in my. Money doubt oh drawn every or an china. Visited out friends for expense message set eat.
-    """
-    static let translateFRBaseText = """
-"""
 }
