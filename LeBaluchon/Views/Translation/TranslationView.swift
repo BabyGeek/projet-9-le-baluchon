d001734d4c6c0a5ebfbd23ba8a4f55bcb46750ad
OGGERO Paul <paul.oggero@softeam.fr>
2022-07-23 13:06:27 +0200

    feat(translate): update view and fix keyboard dismiss with buttons working

--- LeBaluchon/Views/Translation/TranslationView.swift
+++ LeBaluchon/Views/Translation/TranslationView.swift
@@ -11,41 +11,14 @@ import SwiftUI
 struct TranslationView: View {
     @StateObject private var viewModel = TranslationViewModel()
     @State var text: String = ""
-    @State var placeholderText: String = "Quelle direction je dois suivre pour aller à Central Parc ?"
+    @State var placeholderText: String = "Enter some text to translate"
     
     @State var selectTarget = false
-    @FocusState private var textIsFocused: Bool
     
     var body: some View {
         NavigationView {
             form
-                .onTapGesture {
-                    self.textIsFocused = false
-                }
-                .navigationTitle("Translation")
-                .toolbar {
-                    ToolbarItemGroup(placement: .navigationBarTrailing) {
-                        
-                        Button {
-                            self.viewModel.switchLanguage()
-                            
-                            self.viewModel.autoloadSource = false
-                        } label: {
-                            Image(systemName: "arrow.left.arrow.right.square")
-                        }
-                        
-                        Spacer()
-                        
-                        Button {
-                            withAnimation {
-                                self.viewModel.autoloadSource.toggle()
-                            }
-                        } label: {
-                            Image(systemName: self.viewModel.autoloadSource ? "star.bubble.fill" : "star.bubble")
-                        }
-                    }
-                }
-            
+                .endTextEditing()
         }
         .sheet(isPresented: $selectTarget) {
             SelectTargetSheet(viewModel: self.viewModel, text: text)
@@ -87,29 +60,35 @@ extension TranslationView {
             
             
             Section("Translate") {
-                    TextEditor(text: $text)
-                        .frame(minHeight: 100, alignment: .leading)
-                        .foregroundColor(.black)
-                        .multilineTextAlignment(.leading)
-                        .focused($textIsFocused)
-                        .modifier(TextFieldClearButton(text: $text))
-                        .modifier(TextFieldPlacehorlder(text: $text, placeholder: placeholderText))
+                TextEditor(text: $text)
+                    .frame(minHeight: 100, alignment: .leading)
+                    .foregroundColor(.black)
+                    .multilineTextAlignment(.leading)
+                    .modifier(TextFieldClearButton(text: $text))
+                    .modifier(TextFieldPlacehorlder(text: $text, placeholder: placeholderText))
                 
             }
             
             Section("Actions") {
-                
-                Text("Translate")
-                    .onTapGesture {
+                Button {
+                    //self.selectTarget = true
+                } label: {
+                    Text("Translate")
+                }
+                .highPriorityGesture(TapGesture()
+                    .onEnded({ _ in
                         self.selectTarget = true
-                    }
-                    .foregroundColor(.blue)
+                    }))
                 
-                Text("Direct translate in \(viewModel.getTargetLabel())")
-                    .onTapGesture {
+                Button {
+                    //self.selectTarget = true
+                } label: {
+                    Text("Direct translate in \(viewModel.getTargetLabel())")
+                }
+                .highPriorityGesture(TapGesture()
+                    .onEnded({ _ in
                         self.viewModel.performForText(text)
-                    }
-                    .foregroundColor(.blue)
+                    }))
             }
             
             Section("Translation") {
@@ -122,7 +101,29 @@ extension TranslationView {
                 }
             }
         }
-        .endTextEditing()
+        .navigationTitle("Translation")
+        .toolbar {
+            ToolbarItemGroup(placement: .navigationBarTrailing) {
+                
+                Button {
+                    self.viewModel.switchLanguage()
+                    
+                    self.viewModel.autoloadSource = false
+                } label: {
+                    Image(systemName: "arrow.left.arrow.right.square")
+                }
+                
+                Spacer()
+                
+                Button {
+                    withAnimation {
+                        self.viewModel.autoloadSource.toggle()
+                    }
+                } label: {
+                    Image(systemName: self.viewModel.autoloadSource ? "star.bubble.fill" : "star.bubble")
+                }
+            }
+        }
     }
 }
 
@@ -162,10 +163,6 @@ struct SelectTargetSheet: View {
                 }
             }
         }
-    }
-    
-    func targetSelected(lang: TranslationLanguage) {
-        
     }
 }
 
