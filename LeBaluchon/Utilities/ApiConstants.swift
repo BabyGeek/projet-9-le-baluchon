5624def70c58d50244c0c1fd6a37b6e18800d7c0
MrSniikyz <babygeek83bis@gmail.com>
2022-08-04 09:48:40 +0200

    Update ApiConstants.swift
    Removed API keys from public repo

--- LeBaluchon/Utilities/ApiConstants.swift
+++ LeBaluchon/Utilities/ApiConstants.swift
@@ -10,11 +10,8 @@ import Foundation
 /// API constants
 struct ApiConstants {
     static let weatherAPIURL = "https://api.openweathermap.org/data/2.5/weather"
-    static let weatherAPIKEY = "68afd04a325c72d8726f2cc2672a78c6"
 
     static let currencyAPIURL = "https://v6.exchangerate-api.com/v6/{apiKey}/{resource}"
-    static let currencyAPIKEY = "d24bf18cfff3beaa662a3323"
 
     static let translationAPIURL = "https://translation.googleapis.com/language/translate/v2"
-    static let translationAPIKEY = "AIzaSyBkcs1EDaphfKpPH2Zv5qVlsAzowjOMorg"
 }
