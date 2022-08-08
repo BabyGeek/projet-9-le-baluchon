0e3b629732cd114e6abb76bec5ace9829514d491
OGGERO Paul <paul.oggero@softeam.fr>
2022-08-08 10:03:01 +0200

    add empty api keys to let app works with no keys

--- LeBaluchon/Utilities/ApiConstants.swift
+++ LeBaluchon/Utilities/ApiConstants.swift
@@ -10,8 +10,11 @@ import Foundation
 /// API constants
 struct ApiConstants {
     static let weatherAPIURL = "https://api.openweathermap.org/data/2.5/weather"
+    static let weatherAPIKEY = ""
 
     static let currencyAPIURL = "https://v6.exchangerate-api.com/v6/{apiKey}/{resource}"
+    static let currencyAPIKEY = ""
 
     static let translationAPIURL = "https://translation.googleapis.com/language/translate/v2"
+    static let translationAPIKEY = ""
 }
